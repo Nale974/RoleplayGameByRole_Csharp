4952e9f669ea502336a4f7d8ad82c59662c57472
Nathan <l.nathan97460@gmail.com>
2021-11-09 14:53:53 +0100

    LEBON_Nathan_DM_IPI_2021_2022 V0.1

---
+++ Model/Character.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LEBON_Nathan_DM_IPI_2021_2022.Model
+{
+    class Character
+    {
+        public String name;
+        public int attack;
+        public int defense;
+        public int initiative;
+        public int damages;
+        public int maximumLife;
+        public int currentLife;
+        public int currentAttackNumber;
+        public int totalAttackNumber;
+
+        private readonly Random random = new Random();
+
+        public Character()
+        {
+        }
+
+        public Character(string name, int attack, int defense, int initiative, int damages, int maximumLife, int currentLife, int currentAttackNumber, int totalAttackNumber)
+        {
+            this.name = name;
+            this.attack = attack;
+            this.defense = defense;
+            this.initiative = initiative;
+            this.damages = damages;
+            this.maximumLife = maximumLife;
+            this.currentLife = currentLife;
+            this.currentAttackNumber = currentAttackNumber;
+            this.totalAttackNumber = totalAttackNumber;
+        }
+
+        public int CalculJetAttack()
+        {
+            return this.attack + random.Next(0, 100);
+        }
+
+        public int CalculJetDefense()
+        {
+            return this.defense+ random.Next(0, 100);
+        }
+
+        public int CalculJetInitiative()
+        {
+            return this.defense + random.Next(0, 100);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,139 @@
+﻿using LEBON_Nathan_DM_IPI_2021_2022.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LEBON_Nathan_DM_IPI_2021_2022
+{
+    class Program
+    {
+        static Random random = new Random();
+        static void Main(string[] args)
+        {
+            Duel();
+        }
+        
+        //Exercice 1 : Duel
+        private static void Duel()
+        {
+            Model.Character character1 = new Model.Character("Hector", 75, 75, 75, 30, 200, 200, 2, 2);
+            Model.Character character2 = new Model.Character("Simon", 75, 75, 75, 30, 200, 200, 2, 2);
+            List<Model.Character> characters = new List<Model.Character>() { character1,character2 };
+
+            //Tant que la vie d'un des personnage est positif, on lance un nouveau round
+            var roundNumber = 1;
+            var playerWin = false;
+
+            Console.WriteLine("\nBilan point de vie:");
+            characters.ForEach(c => Console.WriteLine(c.name + " : " + c.currentLife));
+
+            while (playerWin == false)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.BackgroundColor = ConsoleColor.White;
+                Console.WriteLine("\n Round " + roundNumber + " :");
+                Console.ResetColor();
+                Console.ReadLine();
+                NewRound(characters);
+                foreach (var character in characters)
+                {
+                    if (character.currentLife <= 0){playerWin = true;}
+                }
+                roundNumber += 1;
+            }
+        }
+
+        private static void NewRound(List<Model.Character> characters)
+        {
+            //Rétablissement des niveaux d'attaque par défaut
+            characters.ForEach(c => c.currentAttackNumber = c.totalAttackNumber);
+
+            //Suivi nombre d'attaque
+            Console.WriteLine("DEBUG - Nombre d'attaque : ");
+            characters.ForEach(c => Console.WriteLine("     "+c.name+" : "+c.currentAttackNumber));
+            Console.WriteLine("");
+
+            //Calcul d'initiative
+            List<Character> charactersOrderByInitiative = CalculInitiative(characters);
+            
+            //Pour chaque personnage du round
+            for (int i = 0; i < charactersOrderByInitiative.Count(); i++)
+            {
+                //Jusqu'a que le personnage n'a plus d'attaque de disponible
+                while (charactersOrderByInitiative[i].currentAttackNumber > 0)
+                {
+                    //Choisi un adversaire aléatoirement dans le reste de la liste
+                    int idAdversaire = i;
+                    while (idAdversaire == i)
+                    {
+                        idAdversaire = random.Next(0, charactersOrderByInitiative.Count());
+                    }
+                    Console.WriteLine("\n"+((charactersOrderByInitiative[i].totalAttackNumber + 1) - charactersOrderByInitiative[i].currentAttackNumber) + "e attaque de " + charactersOrderByInitiative[i].name 
+                        +", "+ charactersOrderByInitiative[i].name + " attaque " + charactersOrderByInitiative[idAdversaire].name + " : ");
+
+                    //Calcul du jet d'attaque de l'attaquant
+                    int jetAttack = charactersOrderByInitiative[i].CalculJetAttack();
+
+                    //Calcul du jet de défense du défenseur
+                    int jetDefense = charactersOrderByInitiative[idAdversaire].CalculJetDefense();
+
+                    //Marge d'attaque
+                    int attackMargin = jetAttack - jetDefense;
+
+                    //Si la marge d'attaque est positif
+                    Console.Write("DEBUG - Jet d'attaque de " + charactersOrderByInitiative[i].name + " = " + jetAttack + " et jet de défense de " + charactersOrderByInitiative[idAdversaire].name + " = " + jetDefense + "\n");
+                    if (attackMargin > 0)
+                    {
+                        int damageSuffered = attackMargin * charactersOrderByInitiative[i].damages / 100;
+                        charactersOrderByInitiative[idAdversaire].currentLife -= damageSuffered;
+                        Console.WriteLine("DEBUG - " + charactersOrderByInitiative[i].name + " inflige " + attackMargin + "*" + charactersOrderByInitiative[i].damages + "/100 , soit " + damageSuffered + " de dommage.");
+                        Console.Write(charactersOrderByInitiative[i].name + " réussi son attaque, " + charactersOrderByInitiative[idAdversaire].name + " perd " + damageSuffered + " point de vie.\n");
+                    }
+                    else //Si négatif
+                    {
+                        String response = charactersOrderByInitiative[i].name + " à raté son attaque. ";
+                        if (charactersOrderByInitiative[idAdversaire].currentAttackNumber > 0)
+                        {
+                            int counterAttack = charactersOrderByInitiative[idAdversaire].damages + random.Next(0, 100)+ (attackMargin * -1);
+                            Console.Write(response + charactersOrderByInitiative[i].name + " à raté son attaque. " + charactersOrderByInitiative[idAdversaire].name + " contre-attaque avec " + counterAttack + " de dégat.\n");
+                            charactersOrderByInitiative[idAdversaire].currentAttackNumber--;
+                        }
+                        else
+                        {
+                            Console.Write(response + charactersOrderByInitiative[i].name + " à raté son attaque. " + charactersOrderByInitiative[idAdversaire].name + " n'a plus d'attaque disponible pour contre-attaquer.\n");
+                        }
+                    }
+
+                    //incrément du nombre d'attaques restantes
+                    charactersOrderByInitiative[i].currentAttackNumber--;
+                }
+            }
+
+            Console.WriteLine("\nBilan point de vie:");
+            charactersOrderByInitiative.ForEach(c => Console.WriteLine(c.name+" : "+c.currentLife));
+        }
+
+        private static List<Character> CalculInitiative(List<Model.Character> characters)
+        {
+            Dictionary<int, Character> charactersWithInitiative = new Dictionary<int, Character>();
+            Console.WriteLine("DEBUG - Jet d'initiative : ");
+            foreach (var character in characters)
+            {
+                //Console.WriteLine(character.Name + " " + (character.Initiative + random.Next(1, 100)));
+                int newInitiative = character.initiative + random.Next(1, 100);
+                while (charactersWithInitiative.ContainsKey(newInitiative))
+                {
+                    newInitiative = character.initiative + random.Next(1, 100);
+                }
+                charactersWithInitiative.Add(newInitiative, character);
+                Console.WriteLine("     "+character.name+" : "+newInitiative);
+            }
+
+            List<Character> charactersOrderByInitiative = charactersWithInitiative.OrderByDescending(c => c.Key)
+                .Select(c => c.Value)
+                .ToList();
+
+            return charactersOrderByInitiative;
+        }
+    }
+}
